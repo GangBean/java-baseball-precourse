db271372ded96ed3ccd123c0b80d5c0d89a10074
Gangbean <xbox2537@gamil.com>
2022-04-15 20:39:49 +0900

    feat : 입/출력 담당 view 컨트롤러 생성 - 기능 목록 내 UI 기능 담당 - 숫자 입력 안내 - 판정 결과 출력 - 게임 종료 여부 입력 안내

---
+++ src/main/java/userinterface/UserInterfaceController.java
@@ -0,0 +1,27 @@
+package userinterface;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class UserInterfaceController {
+    public UserInterfaceController() {
+    }
+
+    public String enterNumber(){
+        System.out.print("숫자를 입력해주세요 : ");
+        return Console.readLine();
+    }
+
+    public void judgeCall(String judge){
+        System.out.println(judge);
+    }
+
+    public void endCall(){
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
+    public String replay(){
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        return Console.readLine();
+    }
+
+}
