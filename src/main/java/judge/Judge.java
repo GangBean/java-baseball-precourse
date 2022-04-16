c403959f1c25eb5e6c8bb8de680df846c6d050d3
Gangbean <xbox2537@gamil.com>
2022-04-16 18:39:32 +0900

    feat : Judge 클래스 생성
    - 역할 : 게임의 정답 생성 및 입력값 검증/판정
    - 기능 :
        1. 정답 생성
            ㄱ. 랜덤수 생성
            ㄴ. 정답 저장
        2. 입력값 검증
            ㄱ. 입력값 자릿수 정상 여부 체크
            ㄴ. 숫자 여부 체크
        3. 입력값 판정
            ㄱ. 볼 판정
            ㄴ. 스트라이크 판정
        4. 판정 결과 생성
            ㄱ. 볼/스트라이크/낫싱 생성

---
+++ src/main/java/judge/Judge.java
@@ -0,0 +1,102 @@
+package judge;
+
+import ball.Ball;
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Judge {
+    private static final int BALL_COUNT = 3;
+    private static final int MAX_BALL_NUMBER = 9;
+    private static final int MIN_BALL_NUMBER = 1;
+    private int[] balls;
+    private int ballCount;
+    private int strikeCount;
+
+    public Judge() {
+        createNumber();
+    }
+
+    private static void validateNumberLength( String input ){
+        if( input.length() != BALL_COUNT ){
+            throw new IllegalArgumentException("Not enough length");
+        }
+    }
+
+    public void createNumber(){
+        this.balls = new int[BALL_COUNT];
+        for (int i = 0; i < BALL_COUNT; i++) {
+            this.balls[i] = Randoms.pickNumberInRange( MIN_BALL_NUMBER, MAX_BALL_NUMBER );
+        }
+    }
+
+    private static void validateNumberFormat( String input ){
+        for( char number: input.toCharArray() ){
+            validateEachNumberFormat(number);
+        }
+    }
+
+    private static void validateEachNumberFormat( char number ){
+        if( number < '0' || number > '9'){
+            throw new IllegalArgumentException( "Not a number format input" );
+        }
+    }
+
+    public String judge( String input ){
+        validateNumberLength( input );
+        validateNumberFormat( input );
+        Ball[] inputBalls = new Ball[ BALL_COUNT ];
+        for( int i = 0; i < BALL_COUNT; i++ ){
+            inputBalls[i] = new Ball( input.charAt(i) - '0', i );
+        }
+        judgeBalls( inputBalls );
+
+        return this.makeCall();
+    }
+
+    private String makeCall(){
+        StringBuilder stringBuilder = new StringBuilder();
+        if( this.ballCount == 0 && this.strikeCount == 0 ){
+            stringBuilder.append( "낫싱" );
+        }
+        if( this.ballCount > 0 ){
+            stringBuilder.append( this.ballCount ).append("볼").append(" ");
+        }
+        if( this.strikeCount > 0 ){
+            stringBuilder.append( this.strikeCount ).append("스트라이크");
+        }
+        this.ballCount = 0;
+        this.strikeCount = 0;
+        return stringBuilder.toString();
+    }
+
+    private void judgeBalls( Ball[] balls ){
+        for( Ball ball : balls ){
+            this.judgeStrike( ball );
+            this.judgeBall( ball );
+        }
+    }
+
+    public int getBallCount() {
+        return ballCount;
+    }
+
+    public int getStrikeCount() {
+        return strikeCount;
+    }
+
+    private void judgeStrike( Ball ball ) {
+        int number = ball.getNumber();
+        int idx = ball.getIdx();
+        if( number == this.balls[idx] ){
+            this.strikeCount++;
+        }
+    }
+
+    private void judgeBall( Ball ball ) {
+        int number = ball.getNumber();
+        int idx = ball.getIdx();
+        if( number == this.balls[ (idx + 1) % BALL_COUNT ]
+            || number == this.balls[ (idx + 2) % BALL_COUNT ] ){
+            this.ballCount++;
+        }
+    }
+}
