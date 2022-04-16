9b658c1bcd9f184b0f6c5bc5bba70c30411ec82b
Gangbean <xbox2537@gamil.com>
2022-04-16 21:03:18 +0900

    feat : 애플리케이션 시작점 추가
    - 게임플레이컨트롤러 객체 생성 및 프로그램 시작점 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,11 @@
 package baseball;
 
+import controller.GamePlayController;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        GamePlayController gamePlayController = new GamePlayController();
+        gamePlayController.startBaseBall();
     }
 }
