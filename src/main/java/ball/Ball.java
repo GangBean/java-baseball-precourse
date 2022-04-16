061603987577b6f8f01b7693b1fc974c1e43f659
Gangbean <xbox2537@gamil.com>
2022-04-16 18:29:23 +0900

    feat : Ball 클래스 생성
    - 역할 : 입력값의 각 자릿수별 숫자 정보 관리
    - 기능
        1. 입력값 자릿수 체크
        2. 숫자 여부 체크

---
+++ src/main/java/ball/Ball.java
@@ -0,0 +1,37 @@
+package ball;
+
+public class Ball {
+    private static final int MAX_BALL_NUMBER = 9;
+    private static final int MIN_BALL_NUMBER = 1;
+    private static final int MAX_INDEX_NUMBER = 2;
+    private static final int MIN_INDEX_NUMBER = 0;
+    private int number;
+    private int idx;
+
+    public Ball(int number, int idx) {
+        validateNumberRange( number );
+        validateIndexRange( idx );
+        this.number = number;
+        this.idx = idx;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public int getIdx() {
+        return idx;
+    }
+
+    private static void validateNumberRange(int number ){
+        if( number < MIN_BALL_NUMBER || number > MAX_BALL_NUMBER ){
+            throw new IllegalArgumentException( "Out of number range" );
+        }
+    }
+
+    private static void validateIndexRange( int idx ){
+        if( idx < MIN_INDEX_NUMBER || idx > MAX_INDEX_NUMBER ){
+            throw new IllegalArgumentException( "Out of index range" );
+        }
+    }
+}
